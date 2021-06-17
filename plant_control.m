9b2b3c6e0765579b9b4e727897bb9126063db1bf
fabriciojallaza <fabriciojallaza@gmail.com>
2021-06-17 14:17:36 -0400

    last implementations

--- plant_control.m
+++ plant_control.m
@@ -12,15 +12,15 @@ d2=4.7e-005;
 %Plant
 A=[0 1 0 0;-c/th1 -(d1+d)/th1 -c/th1 -d/th1;0 0 0 1; -c/th2 -d/th2 -c/th2 -(d2+d)/th2];
 B=[0 ;kt1/th1 ; 0 ; 0 ]; % we are interested in the first motor torque
-C=[ 0 0 1 0]; % we are intersted in the secon motor output/angle
+C=[ 0 0 1 0]; % we are intersted in the second motor output/angle
 D=[0];
 plant=ss(A,B,C,D);
 
 %% Controllability and observability
 Co = ctrb(A,B);
 Ob = obsv(A,C);
-rc = rank(Co)
-ro = rank(Ob)
+Rango_Controlablidad = rank(Co)
+Rango_Observabilidad = rank(Ob)
 %With a rank of 4 each we see that are controlable and observable
 %% Requirements
 OS=6; %overshoot less than 6%
@@ -54,8 +54,9 @@ m=m(1);
 % a, b extended matrix
 Aext=[A zeros(n,1); -C zeros(m,1)];
 Bext=[B; -D];
-% Controllability of extended matrix
-Co_ext = ctrb(Aext,Bext);
+% Contrabilidad Matrices extendidas
+Coext = ctrb(Aext,Bext);
+Rango_Controlablidad_Mext = rank(Coext)
 
 p5=real(p1)*10-1;
 kext=place(Aext,Bext,[p1,p2,p3,p4,p5]);
@@ -95,22 +96,17 @@ kiext=place(Aext,Bext,[pin1,pin2,pin3,pin4,pin5]);
 ki2=kiext(1:n);
 kei=kiext(n+1);
 plant_ucitaei=ss([A-B*ki2 -B*kei;-C+D*ki2 D*kei],[zeros(n,1); 1],[C 0],0);
-%% LQR
-Q =  [100   0     0     0;
-      0     0     0     0;
-      0     0     100     0;
-      0     0     0     0];
- 
-R = 0.001;
 
-[klqr,S,E] = lqr(A,B,Q,R);
-Kpre=inv(-(C-D*klqr)*inv(A-B*klqr)*B+D);
-
-plant_uclqr= ss((A - B*Kpre), B, C, D);
 %% State Observer
 % Design
 PolesObs = p(1:n)*10;
 L = place(A',C', PolesObs)'; %ganancia del obs
+
+% Observer Space State
+Aobs = A - L*C;
+Bobs = [B-L*D L];
+Cobs = eye(n);
+Dobs = zeros(n,2);
 
 % Observer + Controller Space State
 Aoc = A-L*C-B*kn+L*D*kn;
@@ -129,40 +125,37 @@ plant_uc_obscosc=ss(Aocc,Bocc,Cocc,Docc);
 %% table generations
 [y1,t1]=step(plant);plant_r=stepinfo(y1,t1);
 [y2,t2]=step(plant_uc);plant_uc_r=stepinfo(y2,t2);
-% [y3,t3]=step(plant_uckpre);plant_uckpre_r=stepinfo(y3,t3);
+
 [y4,t4]=step(plant_uci);plant_uci_r=stepinfo(y4,t4);
 [y5,t5]=step(plant_ucitae);plant_ucitae_r=stepinfo(y5,t5);
-% [y6,t6]=step(plant_ucitaek);plant_ucitaek_r=stepinfo(y6,t6);
+
 [y7,t7]=step(plant_ucitaei);plant_ucitaei_r=stepinfo(y7,t7);
-[y8,t8]=step(plant_uclqr);plant_uclqr_r=stepinfo(y8,t8);
+
 [y9,t9]=step(plant_uc_obscosc);plant_uc_obscosc_r=stepinfo(y9,t9);
 
-ControlType={'Plant';'POLE PLACEMENT';'POLE PLACEMENT INTEGRAL';'ITAE';'ITAE INTEGRAL';'LQR';'STATE OBSERVER'};
-RiseTime={plant_r.RiseTime;plant_uc_r.RiseTime;plant_uci_r.RiseTime;plant_ucitae_r.RiseTime;plant_ucitaei_r.RiseTime;plant_uclqr_r.RiseTime;plant_uc_obscosc_r(1).RiseTime};
-SettlingTime={plant_r.SettlingTime;plant_uc_r.SettlingTime;plant_uci_r.SettlingTime;plant_ucitae_r.SettlingTime;plant_ucitaei_r.SettlingTime;plant_uclqr_r.SettlingTime;plant_uc_obscosc_r(1).SettlingTime};
-Overshoot={plant_r.Overshoot;plant_uc_r.Overshoot;plant_uci_r.Overshoot;plant_ucitae_r.Overshoot;plant_ucitaei_r.Overshoot;plant_uclqr_r.Overshoot;plant_uc_obscosc_r(1).Overshoot};
+ControlType={'Plant';'POLE PLACEMENT';'POLE PLACEMENT INTEGRAL';'ITAE';'ITAE INTEGRAL';'STATE OBSERVER'};
+RiseTime={plant_r.RiseTime;plant_uc_r.RiseTime;plant_uci_r.RiseTime;plant_ucitae_r.RiseTime;plant_ucitaei_r.RiseTime;plant_uc_obscosc_r(1).RiseTime};
+SettlingTime={plant_r.SettlingTime;plant_uc_r.SettlingTime;plant_uci_r.SettlingTime;plant_ucitae_r.SettlingTime;plant_ucitaei_r.SettlingTime;plant_uc_obscosc_r(1).SettlingTime};
+Overshoot={plant_r.Overshoot;plant_uc_r.Overshoot;plant_uci_r.Overshoot;plant_ucitae_r.Overshoot;plant_ucitaei_r.Overshoot;plant_uc_obscosc_r(1).Overshoot};
 
 T=table(ControlType,RiseTime,SettlingTime,Overshoot);
 disp(T)
 
 %% Simulations
-subplot(421);
+subplot(321);
 step(plant), grid on, title('PLANT')
-subplot(422);
+subplot(322);
 step(plant_uc), grid on, title('PLANT - POLE PLACEMENT')
-% subplot(423);
-% step(plant_uckpre), grid on, title('PLANT - POLE PLACEMENT KPRE')
-subplot(423);
+
+subplot(323);
 step(plant_uci), grid on, title('PLANT - POLE PLACEMENT INTEGRAL')
-subplot(424);
+subplot(324);
 step(plant_ucitae), grid on, title('PLANT UNDER CONTROL ITAE')
-% subplot(426);
-% step(plant_ucitaek), grid on, title('PLANT - ITAE KPRE')
-subplot(425);
+
+subplot(325);
 step(plant_ucitaei), grid on, title('PLANT - ITAE INTEGRAL')
-subplot(426);
-step(plant_uclqr), grid on, title('PLANT UNDER CONTROL LQR')
-subplot(4,2,[7,8]);
+
+subplot(326);
 step(plant_uc_obscosc(2)), grid on, title('PLANT UNDER CONTROL STATE OBSERVER')
 
 %% Simulink
